21a854dcdc9184b5784021ac314e99ce135ba11d
paulkass <pkassian@gmail.com>
2016-11-30 19:31:30 -0800

    Added some manual test files

---
+++ src/main/java/com/company/util/SomeVerticle.java
@@ -0,0 +1,14 @@
+package com.company.util;
+
+import io.vertx.core.AbstractVerticle;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class SomeVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() throws Exception {
+
+  }
+}
